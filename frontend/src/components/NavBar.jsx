d04bcbf6bf668265ff173112ee7d5cadab328abb
José Vega <josevega1999.16@gmail.com>
2025-09-16 12:36:02 -0400

    Titulo

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -70,7 +70,7 @@ export default function NavBar() {
             }}
             onClick={handleLogoClick}
           >
-            ProjectHub
+            Prueba JV
           </Typography>
 
           {user ? (
